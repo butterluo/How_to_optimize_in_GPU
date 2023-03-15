dbf993f373674867c2e5b96e2375ce0e2af2802c
bt <you@example.com>
2023-03-15 13:07:20 +0800

    xxx

--- sgemm/sgemm_v3bt.cu
+++ sgemm/sgemm_v3bt.cu
@@ -23,11 +23,11 @@
 // K: ldA
 // N: ldB
 template <
-    const int BLOCK_SIZE_M /* height of block of C that each thread block calculate //128 */,  
-    const int BLOCK_SIZE_K,  // width of block of A that each thread block load into shared memory //8
-    const int BLOCK_SIZE_N,  // width of block of C that each thread block calculate //128
-    const int THREAD_SIZE_Y, // height of block of C that each thread calculate //8
-    const int THREAD_SIZE_X,  // width of block of C that each thread calculate //8
+    const int BLOCK_SIZE_M,  // height of block of C that each thread block calculate
+    const int BLOCK_SIZE_K,  // width of block of A that each thread block load into shared memory
+    const int BLOCK_SIZE_N,  // width of block of C that each thread block calculate
+    const int THREAD_SIZE_Y, // height of block of C that each thread calculate
+    const int THREAD_SIZE_X,  // width of block of C that each thread calculate
     const bool ENABLE_DOUBLE_BUFFER // whether enable double buffering or not
     > 
 __global__ void Sgemm( 
@@ -46,60 +46,67 @@ __global__ void Sgemm(
     int ty = threadIdx.y;
     
     // the threads number in Block of X,Y
-    const int THREAD_X_PER_BLOCK = BLOCK_SIZE_N / THREAD_SIZE_X;//BlkDim.x 16
-    const int THREAD_Y_PER_BLOCK = BLOCK_SIZE_M / THREAD_SIZE_Y;//BlkDim.y 16
-    const int THREAD_NUM_PER_BLOCK = THREAD_X_PER_BLOCK * THREAD_Y_PER_BLOCK;//BTBT 一个blk有256=16*16个thrd
+    const int THREAD_X_PER_BLOCK = BLOCK_SIZE_N / THREAD_SIZE_X;
+    const int THREAD_Y_PER_BLOCK = BLOCK_SIZE_M / THREAD_SIZE_Y;
+    const int THREAD_NUM_PER_BLOCK = THREAD_X_PER_BLOCK * THREAD_Y_PER_BLOCK;
 
     // thread id in cur Block
     const int tid = ty * THREAD_X_PER_BLOCK + tx;
 
     // shared memory
-    __shared__ float As[2][BLOCK_SIZE_K][BLOCK_SIZE_M];//[2][8][128]
+    __shared__ float As[2][BLOCK_SIZE_K][BLOCK_SIZE_M];
     __shared__ float Bs[2][BLOCK_SIZE_K][BLOCK_SIZE_N];
     // registers for C
-    float accum[THREAD_SIZE_Y][THREAD_SIZE_X];//[128][128]
+    float accum[THREAD_SIZE_Y][THREAD_SIZE_X];
     #pragma unroll
-    for(int i=0; i<THREAD_SIZE_Y; i++){
+    for(int i=0; i<THREAD_SIZE_Y; i++){//BTBT ??? 考虑用汇编置零,或不置零
         #pragma unroll
         for(int j=0; j<THREAD_SIZE_X; j++){
             accum[i][j]=0.0;
         }
     }
-    
-    // registers load global memory
-    const int ldg_num_a = BLOCK_SIZE_M * BLOCK_SIZE_K / (THREAD_NUM_PER_BLOCK * 4);//BTBT ??? 4是指float4?
+    // registers for A and B
+    float frag_a[2][THREAD_SIZE_Y];
+    float frag_b[2][THREAD_SIZE_X];
+    // registers load global memory//BTBT ??? 要从glb到reg再到smem,这里是暂存用的reg,是否考虑汇编或txture
+    const int ldg_num_a = BLOCK_SIZE_M * BLOCK_SIZE_K / (THREAD_NUM_PER_BLOCK * 4);//BTBT A:每个thrd加载多少个四元组
     const int ldg_num_b = BLOCK_SIZE_K * BLOCK_SIZE_N / (THREAD_NUM_PER_BLOCK * 4);
-    float ldg_a_reg[4*ldg_num_a];//BTBT (128*8)/256=4 每个thrd负责取多少glb的elem
+    float ldg_a_reg[4*ldg_num_a];//A临时存放每个thrd从glb加载过来的数据
     float ldg_b_reg[4*ldg_num_b];
 
-    // threads number in one row (in BlkTil) //BTBT ??? 除4啥意思???每个thrd取一次,每次一个float4?
-    const int A_TILE_THREAD_PER_ROW = BLOCK_SIZE_K / 4;//BTBT 每thrd每次取float4的话,每个blk的一行elem需要多少thrd就能一次取完 8/4=2
+    // threads number in one row
+    const int A_TILE_THREAD_PER_ROW = BLOCK_SIZE_K / 4;//A在行维度需要有多少thrd,每个thrd每次加载float4
     const int B_TILE_THREAD_PER_ROW = BLOCK_SIZE_N / 4;
 
     // row number and col number that needs to be loaded by this thread
-    const int A_TILE_ROW_START = tid / A_TILE_THREAD_PER_ROW;//BTBT 该thrd在BlkTIl内是第几行
+    const int A_TILE_ROW_START = tid / A_TILE_THREAD_PER_ROW;//A在行维度需要A_TILE_THREAD_PER_ROW个thrd的情况下,这个thrd属于第几行
     const int B_TILE_ROW_START = tid / B_TILE_THREAD_PER_ROW;
 
-    const int A_TILE_COL = tid % A_TILE_THREAD_PER_ROW * 4; //BTBT 该thrd在BlkTIl内是第几列
+    const int A_TILE_COL = tid % A_TILE_THREAD_PER_ROW * 4; //A在行维度需要A_TILE_THREAD_PER_ROW个thrd的情况下,这个thrd属于第几列
     const int B_TILE_COL = tid % B_TILE_THREAD_PER_ROW * 4;
 
     // row stride that thread uses to load multiple rows of a tile
-    const int A_TILE_ROW_STRIDE = THREAD_NUM_PER_BLOCK / A_TILE_THREAD_PER_ROW;//BTBT 一个BlkTil内有多少thrd row 256/2=128
+    const int A_TILE_ROW_STRIDE = THREAD_NUM_PER_BLOCK / A_TILE_THREAD_PER_ROW;//A在行维度需要A_TILE_THREAD_PER_ROW个thrd的情况下,一个thrdBlk可安排多少行
     const int B_TILE_ROW_STRIDE = THREAD_NUM_PER_BLOCK / B_TILE_THREAD_PER_ROW;
 
-    A = &A[(BLOCK_SIZE_M * by)* K];//BTBT 指针移动到该BlkTil在A中的左上角起点
-    B = &B[BLOCK_SIZE_N * bx];//BTBT 指针移动到该BlkTil在B中的左上角起点
+    A = &A[(BLOCK_SIZE_M * by)* K];//BTBT bug 没判断有没超范围或padding
+    B = &B[BLOCK_SIZE_N * bx];
+
+    //load index of the tile
+    const int warp_id = tid / 32;
+    const int lane_id = tid % 32;
+    const int tile_index = (warp_id%4)*16 + (lane_id/16)*8 + (lane_id%2)*4;//BTBT bug  B的tile_index的计算可能不是这样的
 
     //transfer first tile from global mem to shared mem
     // load A from global memory to shared memory
     #pragma unroll
-    for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {//BTBT 当前场景下共256thrd,每行2thrd,有128行,256thrd每thrd取一个float4填入SMEM,刚好填满As[0]which is [4*2][128]
+    for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {
         int ldg_index = i / A_TILE_ROW_STRIDE * 4;
-        FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(A[OFFSET(  //OFFSET(row, col, ld) ((row) * (ld) + (col))
-            A_TILE_ROW_START + i, // row (tid/A_TILE_THREAD_PER_ROW=tid/2)+i
-            A_TILE_COL, // col tid % A_TILE_THREAD_PER_ROW * 4
+        FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(A[OFFSET(
+            A_TILE_ROW_START + i, // row
+            A_TILE_COL, // col
             K )]);
-        As[0][A_TILE_COL][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index]; //BTBT ??? 写SMEM是否有bank conflic
+        As[0][A_TILE_COL][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index];    //BTBT bug 这里貌似也有bank conflict,同wrp的lane0和lane1写同一个bank ???
         As[0][A_TILE_COL+1][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+1];
         As[0][A_TILE_COL+2][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+2];
         As[0][A_TILE_COL+3][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+3];
@@ -114,17 +121,8 @@ __global__ void Sgemm(
     }
     __syncthreads();
 
-    // registers for A and B SMEM
-    float frag_a[2][THREAD_SIZE_Y];
-    float frag_b[2][THREAD_SIZE_X];
-
-    //load index of the tile
-    const int warp_id = tid / 32;
-    const int lane_id = tid % 32;
-    const int tile_index = (warp_id%4)*16 + (lane_id/16)*8 + (lane_id%2)*4;
-
     // load A from shared memory to register
-    FETCH_FLOAT4(frag_a[0][0]) = FETCH_FLOAT4(As[0][0][tile_index]);//BTBT 参考https://zhuanlan.zhihu.com/p/481600052 ,这里是取128的前半,下面取后半
+    FETCH_FLOAT4(frag_a[0][0]) = FETCH_FLOAT4(As[0][0][tile_index]);      //当wrpId和laneId不断变化时,其实是在同一行取数,warp0和warp4取得是同样的16个元素。而warp里面，线程0、2、4、6、8、10、12、14是取得同样的4个元素。由于取得是同样的元素，同一个bank触发多播的机制，
     FETCH_FLOAT4(frag_a[0][4]) = FETCH_FLOAT4(As[0][0][tile_index + 64]);
     // load B from shared memory to register
     FETCH_FLOAT4(frag_b[0][0]) = FETCH_FLOAT4(Bs[0][0][tile_index]);
@@ -159,9 +157,9 @@ __global__ void Sgemm(
 
         #pragma unroll
         for(int j=0; j<BLOCK_SIZE_K; ++j){
-            // load next tile from shared mem to register //BTBT ??? 这个load和write的次序貌似错了?tile_index的计算对于A,B来说应该不一样,否则无法计算完整矩阵?这个文件问题较多,不建议看
-            next_stage_flag = (j==BLOCK_SIZE_K-1)?load_stage_idx:write_stage_idx;
-            // load A from shared memory to register
+            // load next tile from shared mem to register 
+            next_stage_flag = (j==BLOCK_SIZE_K-1)?load_stage_idx:write_stage_idx;//BTBT bug 这里的next_stage_flag要一直等于load_stage_idx吧,write_stage_idx是下一轮才写进smem的吧 ???
+            // load A from shared memory to register //BTBT bug 第一轮时貌似不能这样做,因为As[1]和Bs[1]中没有任何数据
             FETCH_FLOAT4(frag_a[(j+1)%2][0]) = FETCH_FLOAT4(As[next_stage_flag][(j+1)%BLOCK_SIZE_K][tile_index]);
             FETCH_FLOAT4(frag_a[(j+1)%2][4]) = FETCH_FLOAT4(As[next_stage_flag][(j+1)%BLOCK_SIZE_K][tile_index + 64]);
             // load B from shared memory to register
@@ -257,7 +255,7 @@ int main(int argc, char** argv) {
     double gigaFlops[2] = {0, 0};
     double flopsPerMatrixMul = 2.0 * M * N * K;
 
-    // don't edit it
+    // don't edit it //BTBT ???
     const int BLOCK_SIZE_M = 128;
     const int BLOCK_SIZE_K = 8;
     const int BLOCK_SIZE_N = 128;
@@ -378,3 +376,9 @@ int main(int argc, char** argv) {
     free(h_C);
     free(h_C1);
 }
+/**
+ * TODO:
+ * 没整除时如何padding
+ * 对half8的支持
+ * 实习生测试那个sample快
+ * */
